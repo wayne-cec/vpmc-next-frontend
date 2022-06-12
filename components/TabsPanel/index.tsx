e6923d05a91a6fa1a57d8b1e0fbe628e914aba5d
MSI\Jim <jim60308@gmail.com>
2022-06-12 13:37:51 +0800

    新增圖表

--- components/TabsPanel/index.tsx
+++ components/TabsPanel/index.tsx
@@ -9,6 +9,7 @@ import { IResult, IResultStatistics } from '../../api/prod'
 import { buildingTypeDecode } from '../CommiteeCard'
 import { ECharts } from 'echarts'
 import ReactEcharts from 'echarts-for-react'
+import { square } from '../MapContainer/AprRegionMap'
 
 export interface ITabsPanel {
   displayData: {
@@ -24,10 +25,6 @@ const TabsPanel = (props: ITabsPanel) => {
   const handleChange = (event: React.SyntheticEvent, newValue: string) => {
     setValue(newValue)
   }
-
-  // useEffect(() => {
-  //   ECharts.
-  // }, [])
 
   return (
     <Box sx={{ width: '100%', typography: 'body1' }}>
@@ -47,17 +44,107 @@ const TabsPanel = (props: ITabsPanel) => {
         </Box>
         {
           Object.keys(props.displayData).map((buildingType, index) => {
-            const option = {
+            const typeData = props.displayData[buildingType]
+            const years = Object.keys(typeData)
+            const meanPriceValues: number[] = []
+            const meanUnitPriceValues: number[] = []
+            const meanAgeValues: number[] = []
+            const countValues: number[] = []
+
+            years.forEach((year) => {
+              const yearD = typeData[year] as IResultStatistics
+              meanPriceValues.push(yearD.priceWithoutParking_MEAN)
+              meanUnitPriceValues.push(yearD.unitPrice_MEAN)
+              meanAgeValues.push(yearD.age_MEAN)
+              countValues.push(yearD.count)
+            })
+
+            const meanPriceOption = {
               xAxis: {
                 type: 'category',
-                data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+                data: years
               },
               yAxis: {
-                type: 'value'
+                type: 'value',
+                name: "單位(百萬)",
+                axisLabel: {
+                  formatter: (function (value: string) {
+                    const newVlaue = Math.round(Number(value) / 1000000)
+                    return newVlaue
+                  }),
+                  align: 'center'
+                }
               },
               series: [
                 {
-                  data: [820, 932, 901, 934, 1290, 1330, 1320],
+                  data: meanPriceValues,
+                  type: 'line',
+                  smooth: true
+                }
+              ]
+            }
+
+            const meanUnitPriceOption = {
+              xAxis: {
+                type: 'category',
+                data: years
+              },
+              yAxis: {
+                type: 'value',
+                name: "單位(萬/坪)",
+                axisLabel: {
+                  formatter: (function (value: string) {
+                    const newVlaue = Math.round(Math.round(Number(value) * square / 1000) / 10)
+                    return newVlaue
+                  }),
+                  align: 'center'
+                }
+              },
+              series: [
+                {
+                  data: meanUnitPriceValues,
+                  type: 'line',
+                  smooth: true
+                }
+              ]
+            }
+
+            const meanAgeOption = {
+              xAxis: {
+                type: 'category',
+                data: years
+              },
+              yAxis: {
+                type: 'value',
+                name: "年"
+              },
+              series: [
+                {
+                  data: meanAgeValues,
+                  type: 'line',
+                  smooth: true
+                }
+              ]
+            }
+
+            const countOption = {
+              xAxis: {
+                type: 'category',
+                data: years
+              },
+              yAxis: {
+                type: 'value',
+                name: "交易數量(千)",
+                axisLabel: {
+                  formatter: (function (value: string) {
+                    return Number(value) / 1000
+                  }),
+                  align: 'center'
+                }
+              },
+              series: [
+                {
+                  data: countValues,
                   type: 'line',
                   smooth: true
                 }
@@ -70,9 +157,14 @@ const TabsPanel = (props: ITabsPanel) => {
               key={index}
             >
               <div className={style.chartsContainer}>
-                <ReactEcharts option={option} />
-                <ReactEcharts option={option} />
-                <ReactEcharts option={option} />
+                <p className={style.title}>平均成交價格</p>
+                <ReactEcharts option={meanPriceOption} />
+                <p className={style.title}>平均成交單價</p>
+                <ReactEcharts option={meanUnitPriceOption} />
+                <p className={style.title}>平均成交屋齡</p>
+                <ReactEcharts option={meanAgeOption} />
+                <p className={style.title}>平均成交數量</p>
+                <ReactEcharts option={countOption} />
               </div>
             </TabPanel>
           })
