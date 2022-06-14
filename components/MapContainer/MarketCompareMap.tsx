06128a913b0e6d777495369e41979734b2c08496
MSI\Jim <jim60308@gmail.com>
2022-06-14 15:32:07 +0800

    幹 開錯release了

---
+++ components/MapContainer/MarketCompareMap.tsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react'
+import style from './index.module.scss'
+import Map from '@arcgis/core/Map'
+import MapView from '@arcgis/core/views/MapView'
+import SpatialReference from "@arcgis/core/geometry/SpatialReference"
+import '@arcgis/core/assets/esri/themes/light/main.css'
+
+export const square = 3.305785
+
+export interface IEsriMap {
+  basemap: string
+}
+
+const AprRegionMap = (props: IEsriMap) => {
+  const [map, setmap] = useState<Map | null>(null)
+  const [view, setview] = useState<MapView | null>(null)
+
+  useEffect(() => {
+    const WGS84 = new SpatialReference({
+      wkid: 4326
+    })
+    const map = new Map({
+      basemap: props.basemap
+    })
+    const mapView = new MapView({
+      map: map,
+      center: [121.4640139307843, 25.013838580240503],
+      zoom: 13,
+      container: 'mapBox-aprRegion',
+      ui: undefined,
+      constraints: {
+        minZoom: 12,
+        maxZoom: 20
+      }
+    })
+    setmap(map)
+    setview(mapView)
+  }, [])
+
+  return (
+    <>
+      <div className={style.esriMapRegion} id='mapBox-aprRegion'>
+      </div>
+    </>
+  )
+}
+
+export default AprRegionMap
