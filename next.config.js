ee50064632f99f9796286582848d4da478569ec7
jimmg35 <jim60308@gmail.com>
2022-08-10 17:27:14 +0800

    asd

--- next.config.js
+++ next.config.js
@@ -6,7 +6,8 @@ const nextConfig = {
   },
   env: {
     customKey: 'my-value',
-    API_DOMAIN_PROD: 'http://140.122.82.98:9085',
+    API_DOMAIN_PROD: 'http://140.122.82.98',
+    API_DOMAIN_STAG: 'http://140.122.82.98:9085',
     API_DOMAIN_DEV: 'http://localhost:9085'
   },
 }
