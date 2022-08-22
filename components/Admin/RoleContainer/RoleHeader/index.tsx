f2faf72d6839ce0dcb4dec09b99ae5731b3a67bf
jimmg35 <jim60308@gmail.com>
2022-08-23 00:59:57 +0800

    asd

--- components/Admin/RoleContainer/RoleHeader/index.tsx
+++ components/Admin/RoleContainer/RoleHeader/index.tsx
@@ -12,11 +12,21 @@ const RoleHeader = ({
 }: IRoleHeader) => {
   return (
     <div className={style.RoleHeader}>
-
       <div className={style.Header}>
-
-        {/* <Typography></Typography> */}
-        <p><AccountCircleIcon sx={{ width: '55px', height: '55px' }} />{roleInfo.name}</p>
+        <AccountCircleIcon sx={{ width: '40px', height: '40px' }} />
+        <p>{roleInfo.name}</p>
+      </div>
+      <div className={style.ChipContainer}>
+        {
+          roleInfo.apps.map((app, index) => {
+            return <div
+              key={index}
+              className={style.Chip}
+            >
+              {app.name}
+            </div>
+          })
+        }
       </div>
     </div>
   )
