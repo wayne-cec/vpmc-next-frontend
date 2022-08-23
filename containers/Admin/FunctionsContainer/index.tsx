0a89ec260df9afe0788084c64a8814a5d6c85a1c
jimmg35 <jim60308@gmail.com>
2022-08-23 22:52:51 +0800

    修整admin 404 page

--- containers/Admin/FunctionsContainer/index.tsx
+++ containers/Admin/FunctionsContainer/index.tsx
@@ -1,20 +1,31 @@
 import Header from '../../../components/Admin/Header'
 import WithSideBarProtected from '../../../layout/admin-layout/WithSideBarProtected'
-import { Box } from '@mui/material'
 import { useDispatch } from 'react-redux'
 import { onToggle } from '../../../store/slice/sideBar'
+import { Box, Typography } from '@mui/material'
+import Image from 'next/image'
+import Link from 'next/link'
 
 const FunctionsContainer = () => {
   const dispatch = useDispatch()
   return (
     <Box sx={{ flex: 1, display: 'flex', flexDirection: 'column' }}>
-      <Header
+      {/* <Header
         title="功能列表"
         onDrawerToggle={() => {
           dispatch(onToggle(true))
         }}
-      />
-      <Box sx={{ flex: 1, py: 6, px: 4, bgcolor: '#eaeff1' }}>
+      /> */}
+      <Box sx={{
+        flex: 1, py: 6, px: 4, bgcolor: '#009BE5',
+        display: 'flex', flexDirection: 'column', alignItems: 'center',
+        justifyContent: 'center'
+      }}>
+
+        <Image src={'/404.png'} width="256px" height="256px" />
+        <Typography variant="h3" color="white" sx={{ userSelect: 'none', margin: '1rem 0' }}>
+          頁面建設中
+        </Typography>
       </Box>
     </Box>
   )
