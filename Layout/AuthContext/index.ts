2b7a8881e26157af95e8b5c9f3e7ebbf59b1775a
jimmg35 <jim60308@gmail.com>
2022-10-25 12:36:19 +0800

    conflict resolved

--- Layout/AuthContext/index.ts
+++
@@ -1,8 +0,0 @@
-import { createContext, useContext } from "react"
-
-export const AuthContext = createContext<{ isAuthenticated: boolean }>({ isAuthenticated: false })
-
-export const useAuth = () => {
-  return useContext(AuthContext)
-}
-
